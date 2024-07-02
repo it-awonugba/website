8eab7a308cf09d715bcf58ef7434640c4a666712
Isaac Awonugba <it.awonugba@gmail.com>
2024-07-02 11:41:46 +0100

    Completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function updateTime() {
-        const now = new Date();
-        const utcTime = now.toUTCString();
-        const dayOfWeek = now.toLocaleDateString('en-US', { weekday: 'long' });
+  function updateTime() {
+    const now = new Date();
+    const utcTime = now.toUTCString();
+    const dayOfWeek = now.toLocaleDateString("en-US", { weekday: "long" });
 
-        document.getElementById('utcTime').innerText = utcTime;
-        document.getElementById('currentDay').innerText = dayOfWeek;
-    }
+    document.getElementById("utcTime").innerText = utcTime;
+    document.getElementById("currentDay").innerText = dayOfWeek;
+  }
 
-    updateTime();
-    setInterval(updateTime, 1000); // Update time every second
+  updateTime();
+  setInterval(updateTime, 1000);
 });
